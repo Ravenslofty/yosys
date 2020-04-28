eed87d4aa04ef9f4b3779305488ba31da73fb673
Dan Ravensloft <dan.ravensloft@gmail.com>
2020-04-28 20:16:35 +0100

    intel_alm: working multiplier inference

--- techlibs/intel_alm/common/dsp_sim.v
+++ techlibs/intel_alm/common/dsp_sim.v
@@ -1,103 +1,34 @@
-(* abc9_box, lib_whitebox *)
-module MISTRAL_MUL27x27(A, B, Y);
-
-parameter A_SIGNED = 0;
-parameter B_SIGNED = 1;
-parameter A_WIDTH = 27;
-parameter B_WIDTH = 27;
-parameter Y_WIDTH = 54;
-
-input [A_WIDTH-1:0] A;
-input [B_WIDTH-1:0] B;
-output [Y_WIDTH-1:0] Y;
+(* abc9_box *)
+module MISTRAL_MUL27x27(input [26:0] A, input [26:0] B, output [53:0] Y);
 
 specify
     (A *> Y) = 4057;
     (B *> Y) = 4057;
 endspecify
-
-if (B_SIGNED !== 1) begin
-    $error("input B must be signed");
-end
-
-if (A_WIDTH > 27) begin
-    $error("input A is wider than 27 bits");
-end
-
-if (B_WIDTH > 27) begin
-    $error("input B is wider than 27 bits");
-end
 
 assign Y = $signed(A) * $signed(B);
 
 endmodule
 
-(* abc9_box, lib_whitebox *)
-module MISTRAL_MUL18X18(A, B, Y);
-
-parameter A_SIGNED = 0;
-parameter B_SIGNED = 1;
-parameter A_WIDTH = 18;
-parameter B_WIDTH = 18;
-parameter Y_WIDTH = 37;
-
-input [A_WIDTH-1:0] A;
-input [B_WIDTH-1:0] B;
-output [Y_WIDTH-1:0] Y;
-
-localparam A_EXTEND = Y_WIDTH - A_WIDTH;
-localparam B_EXTEND = Y_WIDTH - B_WIDTH;
+(* abc9_box *)
+module MISTRAL_MUL18X18(input [17:0] A, input [17:0] B, output [35:0] Y);
 
 specify
     (A *> Y) = 4057;
     (B *> Y) = 4057;
 endspecify
-
-if (B_SIGNED !== 1) begin
-    $error("input B must be signed");
-end
-
-if (A_WIDTH > 18) begin
-    $error("input A is wider than 18 bits");
-end
-
-if (B_WIDTH > 18) begin
-    $error("input B is wider than 18 bits");
-end
 
 assign Y = $signed(A) * $signed(B);
 
 endmodule
 
-(* abc9_box, lib_whitebox *)
-module MISTRAL_MUL9X9(A, B, Y);
-
-parameter A_SIGNED = 0;
-parameter B_SIGNED = 1;
-parameter A_WIDTH = 9;
-parameter B_WIDTH = 9;
-parameter Y_WIDTH = 18;
-
-input [A_WIDTH-1:0] A;
-input [B_WIDTH-1:0] B;
-output [Y_WIDTH-1:0] Y;
+(* abc9_box *)
+module MISTRAL_MUL9X9(input [8:0] A, input [8:0] B, output [17:0] Y);
 
 specify
     (A *> Y) = 4057;
     (B *> Y) = 4057;
 endspecify
-
-if (B_SIGNED !== 1) begin
-    $error("input B must be signed");
-end
-
-if (A_WIDTH > 9) begin
-    $error("input A is wider than 9 bits");
-end
-
-if (B_WIDTH > 9) begin
-    $error("input B is wider than 9 bits");
-end
 
 assign Y = $signed(A) * $signed(B);
 
